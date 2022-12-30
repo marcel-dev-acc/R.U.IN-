b4841e588e88e3c3449d9ed10a6a7a2607276bd8
Marcel <marcelm4242@gmail.com>
2022-12-30 14:50:38 +0000

    feat: added dark and light red to colours

--- app/src/constants/colours.ts
+++ app/src/constants/colours.ts
@@ -2,6 +2,8 @@ type Colours = {
   BLACK: string;
   WHITE: string;
   RED: string;
+  LIGHT_RED: string;
+  DARK_RED: string;
   LIGHT_BLUE: string;
   DARK_BLUE: string;
 };
@@ -10,6 +12,8 @@ const colours: Colours = {
   BLACK: 'rgb(0,0,0)',
   WHITE: 'rgb(255,255,255)',
   RED: 'rgb(255,0,0)',
+  LIGHT_RED: 'rgb(250,128,114)',
+  DARK_RED: 'rgb(139,0,0)',
   LIGHT_BLUE: 'rgb(164,219,232)',
   DARK_BLUE: 'rgb(0,0,66)',
 };
