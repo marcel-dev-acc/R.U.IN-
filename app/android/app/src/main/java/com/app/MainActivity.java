56ad57b5ed7aca2881644be521f9dcdec4c9b978
Marcel <marcelm4242@gmail.com>
2022-12-30 13:05:57 +0000

    feat: added react native navigation

--- app/android/app/src/main/java/com/app/MainActivity.java
+++ app/android/app/src/main/java/com/app/MainActivity.java
@@ -3,6 +3,7 @@ package com.app;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
+import android.os.Bundle;
 
 public class MainActivity extends ReactActivity {
 
@@ -23,6 +24,14 @@ public class MainActivity extends ReactActivity {
   @Override
   protected ReactActivityDelegate createReactActivityDelegate() {
     return new MainActivityDelegate(this, getMainComponentName());
+  }
+
+  /**
+   * For react navigation
+   */
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
   }
 
   public static class MainActivityDelegate extends ReactActivityDelegate {
