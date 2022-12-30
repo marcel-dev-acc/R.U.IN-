68a407e1e085f31dabd0e5e39062fb691633c6a4
Marcel <marcelm4242@gmail.com>
2022-12-30 14:50:20 +0000

    feat: added auth url to env

--- app/src/constants/env.ts
+++ app/src/constants/env.ts
@@ -2,10 +2,12 @@ import Config from "react-native-config";
 
 type Environment = {
   CHAT_URL: string | undefined;
+  AUTH_URL: string | undefined;
 };
 
 const environment: Environment = {
   CHAT_URL: Config.CHAT_URL,
+  AUTH_URL: Config.AUTH_URL,
 };
 
 export default environment;
